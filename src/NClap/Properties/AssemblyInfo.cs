118672c55041d98d95fbaf6f29bf0993d0ffcf14
Reuben Olinsky <reubeno@users.noreply.github.com>
2017-07-16 21:55:10 -0700

    Updates for build

--- src/NClap/Properties/AssemblyInfo.cs
+++ src/NClap/Properties/AssemblyInfo.cs
@@ -9,7 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("ce1a820d-79f3-410d-b869-884dea01fbe6")]
-[assembly: NeutralResourcesLanguage("en-us")]
 
 [assembly: SuppressMessage("Microsoft.Globalization", "CA1304:SpecifyCultureInfo")]
 [assembly: SuppressMessage("Microsoft.Globalization", "CA1305:SpecifyIFormatProvider")]
