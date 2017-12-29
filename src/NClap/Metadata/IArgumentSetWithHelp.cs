a30bbb9bdd9062c8d522cc1896a7bfa41a603311
Reuben Olinsky <reubeno@users.noreply.github.com>
2017-12-29 07:05:06 -0800

    Add missing file

---
+++ src/NClap/Metadata/IArgumentSetWithHelp.cs
@@ -0,0 +1,14 @@
+﻿namespace NClap.Metadata
+{
+    /// <summary>
+    /// Interface to be implemented on argument set types that expose help options.
+    /// </summary>
+    public interface IArgumentSetWithHelp
+    {
+        /// <summary>
+        /// True if the user wants to receive usage help information; false
+        /// otherwise.
+        /// </summary>
+        bool Help { get; set; }
+    }
+}
